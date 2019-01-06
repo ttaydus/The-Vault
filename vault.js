41598baad6e8e16d047992267c314923cb4dc85d
Tyler <tylertaydus4444@gmail.com>
2019-01-05 15:47:59 -1000

    all passing

--- vault.js
+++ vault.js
@@ -1,4 +1,41 @@
 'use strict';
 module.exports = function() {
+
+var obj = {};
+
+function setValue(key, value){
+    obj[key] = value;
+}
+
+function getValue(key){
+    if(obj[key] === undefined){
+        return null;
+    }else{
+        return obj[key];
+    }
+}
+    
+return{
+    setValue,
+    getValue
+}
+
   
 };
+
+
+
+// 'use strict';
+// module.exports = function() {
+
+//     return{
+//         setValue: (key, value) => (obj[key] = value),
+//         getValue: (key, value) => {
+//                 if(obj[key] === undefined){
+//                     return null;
+//                 }else{
+//                     return obj[key];
+//             }
+//         }
+//     }
+// };
